52afb03522f040dbff17386e72b201c0dce08b7b
Mary Thengvall <mthengvall@Mary-Thengvalls-MacBook-Pro.local>
2015-12-28 14:26:27 -0800

    adding schema

--- db/schema.rb
+++ db/schema.rb
@@ -21,24 +21,6 @@ ActiveRecord::Schema.define(version: 20151017005039) do
     t.string "slack_id"
   end
 
-  create_table "customers", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "customers", ["email"], name: "index_customers_on_email", unique: true, using: :btree
-  add_index "customers", ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true, using: :btree
-
   create_table "settings", force: :cascade do |t|
     t.string "channel_type", default: "group"
     t.string "name",         default: "Standup"
